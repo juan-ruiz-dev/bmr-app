c7973972531c345efaa58e59a0d8fd08893e5f79
Juan Ruiz <jryanruiz@live.com>
2023-01-14 20:54:35 -0700

    Starting calorie counter, updated to add select for exercise amount

--- index.js
+++ index.js
@@ -2,39 +2,77 @@
 const express = require('express');
 const app = express();
 const request = require('request');
-const https = require('https');
+const ejs = require('ejs')
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true}));
+app.set('view engine', 'ejs');
+let bmr = null;
+
+
+
+
+
 
 
 app.get('/', function (req, res){
-    res.sendFile(__dirname + '/index.html')
+    let today = new Date();
+    var options = 
+{ 
+    weekday: 'long', 
+    day: 'numeric', 
+    month: 'long'
+}
+let day = today.toLocaleDateString("en-US", options)    
+
+res.render('index', {currentDay: day, bmr: bmr});
+
 });
 
+app.use(express.static(('public')));
 
-app.use(express.static((__dirname, 'public')));
 
 
 app.post('/', function (req, res) {
-    
 height = req.body.height
 weight = req.body.weight
 age = req.body.age
 gender = req.body.test
+exercise = req.body.exercise
 
 weightInKg = weight * .453592
 heightInCm = height * 2.54
 
-let BMR = null; 
-
 if(gender == "male") {
-BMR = 88.362 + (13.397 * weightInKg) + (4.799 * heightInCm) - (5.677 * age)
+bmr = Math.ceil(88.362 + (13.397 * weightInKg) + (4.799 * heightInCm) - (5.677 * age))
 }else if (gender == "female"){
-    BMR = 447.593 + (9.247 * weightInKg) + (3.098 * heightInCm) - (4.330 * age)
+bmr = Math.ceil(447.593 + (9.247 * weightInKg) + (3.098 * heightInCm) - (4.330 * age))
 }
 
-
-res.sendFile(__dirname + '/success.html')
+if(exercise == "1"){
+    let light = bmr * .10
+    bmr = bmr + light
+}else if(exercise == "2"){
+    let light = bmr * .31
+    bmr = bmr + light
+}else if(exercise == "3"){
+    let light = bmr * .37
+    bmr = bmr + light
+}else if(exercise == "4"){
+    let light = bmr * .43
+    bmr = bmr + light
+}else if(exercise == "5"){
+    let light = bmr * .54
+    bmr = bmr + light
+}else {
+   
+}
+// percent differences based on exercise 
+//1 = 18%
+//2 = 31%
+//3 37%
+//4 43%
+//5 54%
+res.redirect("/")
 
 })
 
